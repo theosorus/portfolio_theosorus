24774112bad8adf9009b5dc1f09e9b4a7e59b189
Gazeux33-IUT <theocastillo@yahoo.com>
2025-02-11 09:55:23 +0100

    update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,27 +10,33 @@ const Skills: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-2/3 bg-amber-50 m-10 rounded-xl flex">
-      <div className="w-1/3 h-full rounded-xl bg-dark-purple">
+    <div className="w-2/3 m-10 rounded-xl flex items-stretch bg-white">
+      <div className="w-1/3 h-full text-white font-domine rounded-xl">
         {Object.keys(skillsData).map((category, index) => (
           <div
             key={category}
-            className={`text-center cursor-pointer p-3  ${
-              activeItem === index ? "bg-light-purple rounded-xl" : ""
+            className={`text-center cursor-pointer p-6 ${
+              activeItem === index
+                ? " rounded-l-xl text-openai-dark-blue"
+                : "bg-openai-dark-blue "
             }`}
             onClick={() => setActiveItem(index)}
           >
-            <h3 className="text-2xl">{category}</h3>
+            <h3 className="text-l">{category}</h3>
           </div>
         ))}
       </div>
 
-      <div className="w-3/4 h-full text-dark-purple grid grid-cols-5 gap-4">
+      <div className="w-3/4 text-openai-dark-blue h-full  grid grid-cols-5 gap-4 rounded-r-xl">
         {Object.values(skillsData)[activeItem].map(
           (skill: { name: string; image: string }) => (
-            <div key={skill.name} className="p-2">
-              <img src={`/skills/${skill.image}`} alt={skill.name} className="w-20 h-20 mx-auto object-contain" />
-              <h4 className="text-xl text-center">{skill.name}</h4>
+            <div key={skill.name} className="p-4">
+              <img
+                src={`/skills/${skill.image}`}
+                alt={skill.name}
+                className="w-20 h-20 mx-auto object-contain"
+              />
+              <h4 className="text-m text-center">{skill.name}</h4>
             </div>
           )
         )}
