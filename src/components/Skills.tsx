6b25963e2aee96d78256e800697aa9688e597e00
Gazeux33-IUT <theocastillo@yahoo.com>
2025-02-11 11:18:59 +0100

    fix

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,15 +10,15 @@ const Skills: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-2/3 m-10 rounded-xl flex items-stretch bg-white">
-      <div className="w-1/3 h-full text-white font-domine rounded-xl">
+    <div className="w-2/3 m-10 rounded-xl flex items-stretch">
+      <div className="w-1/3 text-white font-domine rounded-xl">
         {Object.keys(skillsData).map((category, index) => (
           <div
             key={category}
-            className={`text-center cursor-pointer p-6 ${
+            className={`text-center cursor-pointer p-6 py-7 ${
               activeItem === index
-                ? " rounded-l-xl text-openai-dark-blue"
-                : "bg-openai-dark-blue "
+                ? " rounded-l-xl text-openai-dark-blue bg-white font-main"
+                : ""
             }`}
             onClick={() => setActiveItem(index)}
           >
@@ -26,20 +26,34 @@ const Skills: React.FC = () => {
           </div>
         ))}
       </div>
+  
+      <div className={ `w-3/4 text-openai-dark-blue rounded-xl bg-white
+        ${activeItem === 0 ? " rounded-tl-none" : ""}
+        ${activeItem === Object.keys(skillsData).length - 1 ? " rounded-bl-none" : ""}`
 
-      <div className="w-3/4 text-openai-dark-blue h-full  grid grid-cols-5 gap-4 rounded-r-xl">
-        {Object.values(skillsData)[activeItem].map(
-          (skill: { name: string; image: string }) => (
-            <div key={skill.name} className="p-4">
-              <img
-                src={`/skills/${skill.image}`}
-                alt={skill.name}
-                className="w-20 h-20 mx-auto object-contain"
-              />
-              <h4 className="text-m text-center">{skill.name}</h4>
-            </div>
-          )
-        )}
+      }
+      
+      
+      
+      
+      >
+      <div className="grid grid-cols-5">
+      {Object.values(skillsData)[activeItem].map(
+            (skill: { name: string; image: string }) => (
+              <div key={skill.name} className="p-4 ">
+                <img
+                  src={`/skills/${skill.image}`}
+                  alt={skill.name}
+                  className="w-20 h-20 mx-auto object-contain"
+                />
+                <h4 className="text-m text-center">{skill.name}</h4>
+              </div>
+            )
+          )}
+
+      </div>
+        
+      
       </div>
     </div>
   );
