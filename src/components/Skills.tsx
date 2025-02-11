3ff5d7504906ffc1749a119b0f8a4be08fbaae54
Gazeux33-IUT <theocastillo@yahoo.com>
2025-02-11 17:35:11 +0100

    animation + docker

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,8 +6,7 @@ const Skills = () => {
   const itemRefs = useRef<HTMLDivElement[]>([]);
   const [highlight, setHighlight] = useState({ top: 0, height: 0 });
   const [activeItem, setActiveItem] = useState(0);
-  
-  // Fonction pour assigner la ref pour chaque item
+
   const setItemRef = (el: HTMLDivElement, index: number) => {
     itemRefs.current[index] = el;
   };
@@ -25,11 +24,17 @@ const Skills = () => {
 
   return (
     <div className="w-2/3 m-10 rounded-xl flex items-stretch">
-      <div ref={listRef} className="w-1/3 relative text-white font-domine rounded-xl">
-        {/* Élément de surbrillance animé */}
+      <div
+        ref={listRef}
+        className="w-1/3 relative text-white font-domine rounded-xl"
+      >
         <div
           className="absolute left-0 rounded-l-xl bg-white text-openai-dark-blue font-main transition-all duration-300"
-          style={{ top: highlight.top, height: highlight.height, width: "100%" }}
+          style={{
+            top: highlight.top,
+            height: highlight.height,
+            width: "100%",
+          }}
         />
         {Object.keys(skillsData).map((category, index) => (
           <div
@@ -40,20 +45,30 @@ const Skills = () => {
             className="relative text-center cursor-pointer p-6 py-7 z-10"
             onClick={() => setActiveItem(index)}
           >
-            <h3 className={`text-l ${activeItem === index ? "text-openai-dark-blue font-main" : "text-white "}`}>
+            <h3
+              className={`text-l ${
+                activeItem === index
+                  ? "text-openai-dark-blue font-main"
+                  : "text-white "
+              }`}
+            >
               {category}
             </h3>
           </div>
         ))}
       </div>
 
-      <div className={ `w-3/4 text-openai-dark-blue rounded-xl bg-white
+      <div
+        className={`w-3/4 text-openai-dark-blue rounded-xl bg-white
         ${activeItem === 0 ? " rounded-tl-none" : ""}
-        ${activeItem === Object.keys(skillsData).length - 1 ? " rounded-bl-none" : ""}`
-      }>
-      
-      <div className="grid grid-cols-5">
-      {Object.values(skillsData)[activeItem].map(
+        ${
+          activeItem === Object.keys(skillsData).length - 1
+            ? " rounded-bl-none"
+            : ""
+        }`}
+      >
+        <div className="grid grid-cols-5">
+          {Object.values(skillsData)[activeItem].map(
             (skill: { name: string; image: string }) => (
               <div key={skill.name} className="p-4 ">
                 <img
@@ -65,14 +80,8 @@ const Skills = () => {
               </div>
             )
           )}
-
+        </div>
       </div>
-
-
-      </div>
-
-
-      
     </div>
   );
 };
