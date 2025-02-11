37756190ca3be90f9e6c5d850e1fb982e7f5aa9c
Gazeux33-IUT <theocastillo@yahoo.com>
2025-02-11 12:52:43 +0100

    up

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -40,12 +40,39 @@ const Skills = () => {
             className="relative text-center cursor-pointer p-6 py-7 z-10"
             onClick={() => setActiveItem(index)}
           >
-            <h3 className={`text-l ${activeItem === index ? "text-openai-dark-blue" : "text-white"}`}>
+            <h3 className={`text-l ${activeItem === index ? "text-openai-dark-blue font-main" : "text-white "}`}>
               {category}
             </h3>
           </div>
         ))}
       </div>
+
+      <div className={ `w-3/4 text-openai-dark-blue rounded-xl bg-white
+        ${activeItem === 0 ? " rounded-tl-none" : ""}
+        ${activeItem === Object.keys(skillsData).length - 1 ? " rounded-bl-none" : ""}`
+      }>
+      
+      <div className="grid grid-cols-5">
+      {Object.values(skillsData)[activeItem].map(
+            (skill: { name: string; image: string }) => (
+              <div key={skill.name} className="p-4 ">
+                <img
+                  src={`/skills/${skill.image}`}
+                  alt={skill.name}
+                  className="w-20 h-20 mx-auto object-contain"
+                />
+                <h4 className="text-m text-center">{skill.name}</h4>
+              </div>
+            )
+          )}
+
+      </div>
+
+
+      </div>
+
+
+      
     </div>
   );
 };
