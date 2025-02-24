dd137373d5415674941f925ee2695a36bfd22824
Gazeux33-IUT <theocastillo@yahoo.com>
2025-02-24 12:11:33 +0100

    Update project image paths to use absolute URLs and add GitHub icon to PreviewProjects component

--- src/components/PreviewProjects.tsx
+++ src/components/PreviewProjects.tsx
@@ -1,50 +1,38 @@
 import projectsData from "../data/projects.json"; // Adjust the path as necessary
 import { Project } from "../type";
+import github from "../../public/icons/github.svg";
 
 const PreviewProjects: React.FC = () => {
   const projectsToSee = projectsData.projects.slice(0, 10);
 
   return (
-    <div className="w-1/2 justify-center">
+    <div className="w-3/4 justify-center max-w-[1000px]">
       {projectsToSee.map((p: Project, idx) => {
-        const isEven = idx % 2 === 0;
+
 
         return (
-          <div
-            key={idx}
-            className={`flex mt-20 items-center justify-center ${
-              isEven ? "flex-row" : "flex-row-reverse"
-            }`}
+          <div className="w-100 h-100 bg-cover bg-center bg-no-repeat justify-center"
+            style={{ backgroundImage: `url(${p.image})` ,  opacity:1}}
           >
-            <div
-              className={`rounded-xl z-1 gap-3 w-1/2 ${
-                isEven
-                  ? "mr-[-100px] justify-start text-left"
-                  : "ml-[-100px] justify-end text-right"
-              }`}
-            >
-              <h1 className="text-[#56dcfc] mb-2">
+
+<h1 className="text-[#56dcfc] mb-2">
                 {p.date} • {p.type}
               </h1>
               <h1 className="text-2xl mb-2">{p.title}</h1>
-              <div className="bg-[rgb(41,41,85)] p-4 rounded-xl shadow-[0px_0px_20px_rgba(0,0,0,0.4)] mb-2">
+              <div className="">
                 <p>{p.description}</p>
               </div>
-              <div className={`flex mb-2 ${isEven ? "justify-start" : "justify-end"}`}>
+              <div className={`flex mb-2`}>
                 {p.tags.map((t, idx) => (
-                  <a href={t.url} key={idx} className="mr-2 text-[#56dcfc]">
+                  <a href={t.url} key={idx} className="mr-2 text-[#56dcfc]" target="_blank">
                     {t.name}
                   </a>
                 ))}
               </div>
-            </div>
-
-            <div className="z-0 w-1/2">
-              <img
-                className="w-full h-50 object-cover rounded-xl"
-                src={p.image}
-                alt={p.title}
-              />
+              <div className={`flex`}>
+              <a className="github" href={p.links[1]} target="_blank"> 
+                <img className="w-7 h-7" src={github} alt="GitHub" />
+              </a>
             </div>
           </div>
         );
@@ -54,3 +42,4 @@ const PreviewProjects: React.FC = () => {
 };
 
 export default PreviewProjects;
+
