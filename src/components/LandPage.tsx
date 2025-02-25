b17fee1d50ffcdcbb500f25dd70ac3ed979d5bba
tcastillo-ubuntu <theocastillo@yahoo.com>
2025-02-25 22:08:54 +0100

    Refactor LandPage component: enhance layout with flexbox and update image styling

--- src/components/LandPage.tsx
+++ src/components/LandPage.tsx
@@ -4,10 +4,23 @@
 
 const LandPage = () => {
     return (
-        <div className="">
-        <img src="/lilo.png" alt="" />
+
+        <div className="flex items-center justify-">
+            <div>
+
+            </div>
+            <div className="bg-dark-purple rounded-2xl my-20"
+                style={{
+                    borderRadius: '52% 48% 49% 51% / 70% 71% 29% 30%',
+                    overflow: 'hidden',
+                    boxShadow: 'rgba(0, 0, 0, 0.4) 0px 30px 90px'
+                }}>
+                <img src="/lilo.png" alt="" className="overflow-hidden" />
+            </div>
+
         </div>
+
     );
-    };
+};
 
 export default LandPage;
