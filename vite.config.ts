9a9adf6896a9cf89d8f4ad1671bec8a9bf269a2d
Theo <thoecastillo@yahoo.com>
2025-08-19 15:47:21 +0200

    manage base with serve

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-// Use a conditional base so local dev server doesn't use the GitHub Pages base.
-export default defineConfig(() => {
-
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/portfolio_gazeux/'
+  
   return {
     plugins: [react(), tailwindcss()],
-    base: "/portfolio_gazeux/", // Set the base path for GitHub Pages
+    base,
   }
 })
